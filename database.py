294393d3ff68ee25e7a86b08a0037e695388037f
kapusta-gg <bogdashka04@gmail.com>
2023-12-01 09:41:27 +0300

    Версия 1.0

--- database.py
+++ database.py
@@ -7,20 +7,3 @@ def create_connection(user="user", password="user", host="localhost", db="test3"
                                   database=db)
     return con
 
-
-if __name__ == "__main__":
-    cnx = create_connection()
-    cur = cnx.cursor()
-
-    data = cur.execute("SELECT * FROM student")
-    row = cur.fetchall()
-
-    for r in row:
-        print(r)
-
-    data = cur.execute("SELECT * FROM teacher")
-    row = cur.fetchall()
-
-    for r in row:
-        print(r)
-
