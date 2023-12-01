8ff39148364d7e399d2123c1fb85ed81c3fadd99
kapusta-gg <bogdashka04@gmail.com>
2023-12-01 09:40:11 +0300

    Созданы пустые классы для вставки записей

---
+++ windows/insert_window.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import  QDialog
+
+
+class InsertWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+
+
+class InsertStudentWindow(InsertWindow):
+    def __init__(self):
+        super().__init__()
+
+
+class InsertTeacherWindow(InsertWindow):
+    def __init__(self):
+        super().__init__()
+
+
+class InsertCourseWindow(InsertWindow):
+    def __init__(self):
+        super().__init__()
+
+
+class InsertGroupWindow(InsertWindow):
+    def __init__(self):
+        super().__init__()
